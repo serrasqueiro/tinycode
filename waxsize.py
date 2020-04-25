6bbd7afc1905b6c12e36165a9a9fb74e536e1830
Henrique Moreira <h@serrasqueiro.com>
2020-04-25 21:27:51 +0100

    Added european page sizes

--- waxsize.py
+++ waxsize.py
@@ -15,6 +15,20 @@ _DIMENSIONS = {"100ppi": ((850, 1168), (0.0, -1.0)),
 _DIMENSION_A4 = ("200ppi", "100ppi",
                  )
 
+_DIM_SIZE_A4 = (210.0, 297.0, "mm", "p")
+_DIM_SIZE_A5 = (105.0, 148.0, "mm", "p")
+_DIM_SIZE_A6 = (52.5, 74.0, "mm", "p")
+_DIM_SIZE_A3 = (420.0, 594.0, "mm", "L")  # usual orientation ("L")
+
+_PAGE_SIZES = {"A4": _DIM_SIZE_A4,
+               "A5": _DIM_SIZE_A5,
+               "A6": _DIM_SIZE_A6,
+               "A3": _DIM_SIZE_A3,
+               }
+
+_PAGE_SIZE_METRIC = ("A4", "A5", "A6", "A3",
+                     )
+
 
 def main():
     """ Main basic tests! """
@@ -25,11 +39,50 @@ def main():
             print("{}: {}".format(d, s))
         return True
 
+    def show_sizes(names, page_sizes):
+        for name in names:
+            width, height, unit, orient = page_sizes[name]
+            s_orient = "Portrait" if orient == "p" else "Landscape" if orient == "L" else "?"
+            s_extra = "{:.2f}".format(height / width)
+            shown = "{}x{}{} (usually: {}) {}".format(width, height, unit, s_orient, s_extra)
+            print("\t{}: {}".format(name, shown))
+        return True
+
     args, code = sys.argv[1:], 0
     assert args == []
     dims = _DIMENSION_A4
+    is_ok, used = verify_tups(_DIMENSIONS,
+                              (_DIMENSION_A4,
+                               ))
+    if not is_ok:
+        code = 1
+        print("Bogus:", used)
     show_dims(dims)
+    print("Page sizes (european):")
+    show_sizes(_PAGE_SIZE_METRIC, _PAGE_SIZES)
     sys.exit(code)
+
+
+def verify_tups(dct, dim_tups):
+    """ Verify dimension dictionary and tuples. """
+    assert isinstance(dct, dict)
+    assert isinstance(dim_tups, (list, tuple))
+    used = dict()
+    is_ok, missing = True, []
+    for k in dct.keys():
+        used[k] = 0
+    for tups in dim_tups:
+        for tup in tups:
+            if tup in used:
+                used[tup] += 1
+            else:
+                missing.append(tup)
+    if missing != []:
+        return False, {"@": missing}
+    for _, seen in used.items():
+        if seen <= 0:
+            is_ok = False
+    return is_ok, used
 
 
 def shown_dim(tup, debug=0):
